ba49b35e48601b1c8947a8a5ed98821b2f732f7b
Dennis schafroth <dennis@schafroth.com>
2025-10-02 15:24:27 +0200

    Change to start

--- pyeloverblik/eloverblik.py
+++ pyeloverblik/eloverblik.py
@@ -272,7 +272,7 @@ class Eloverblik:
                                 float(i['out_Quantity.quantity']))
 
                         date = datetime.strptime(
-                            period['timeInterval']['end'], '%Y-%m-%dT%H:%M:%S%z')
+                            period['timeInterval']['start'], '%Y-%m-%dT%H:%M:%S%z')
 
                         time_series = TimeSeries(200, date, metering_data)
 
